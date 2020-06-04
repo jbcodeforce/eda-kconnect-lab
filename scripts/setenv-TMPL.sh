c6d1e795b0fc919a1a283b9f094a95a69ee5b66d
Jerome Boyer <boyerje@us.ibm.com>
2020-06-03 21:40:23 -0700

    Add setenv template

---
+++ scripts/setenv-TMPL.sh
@@ -0,0 +1,6 @@
+# SCENARIO 1 & 2 local to event streams
+# localhost kafka cluster as source
+# export KAFKA_BROKERS=kafka:9092
+# event streams as target cluster
+export KAFKA_BROKERS=
+export KAFKA_APIKEY=
